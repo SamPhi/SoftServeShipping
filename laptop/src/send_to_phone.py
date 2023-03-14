b3f7533000f9d0b5dbf968d062550e0a01e4c37e
Sam Phillips <sam_phillips@berkeley.edu>
2023-03-14 15:58:55 -0700

    Added comments to send_to_phone.py

--- laptop/src/send_to_phone.py
+++ laptop/src/send_to_phone.py
@@ -12,22 +12,20 @@ LS2 = False
 theta = 12
 positionalData = [pos_x,pos_y,LS1,LS2,theta]
 
+
+#start_server function begins connection to phone. To be called once on startup
 def start_server():
-	global client_socket
+	global client_socket #Must define as global to be accessible in other functions
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client_socket.connect((host, port))
 
-
+#write_Phone function sends data to laptop. To be called multiple times during operation
 def write_Phone(positioanlData):
-	global client_socket
-	positionalDataArr = json.dumps({"pos_x": pos_x, "pos_y":pos_y,"LS1":LS1,"LS2":LS2,"theta":theta})
-	#for s in positionalDataArr:
-	client_socket.send(positionalDataArr.encode())
-		#client_socket.sendall(repr(s).encode('utf-8'))
-#			print("Ran")
-	# Close the connection
-	client_socket.close()
+	global client_socket #must define as global to be accessible in other functions
+	positionalDataArr = json.dumps({"pos_x": pos_x, "pos_y":pos_y,"LS1":LS1,"LS2":LS2,"theta":theta}) #uses Json to create dictionary that can be read by phone at other end
+	client_socket.send(positionalDataArr.encode()) #sends data
+	client_socket.close() #closes connection - to be deleted?
 
+#Calls functions for testing purposes
 start_server()
 write_Phone(positionalData)
-#Add global clientsocket
