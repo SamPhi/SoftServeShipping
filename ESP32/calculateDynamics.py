c39526f6560c06b0ccd924641872963569039e93
tijmensep <tijmensep@gmail.com>
2023-04-04 12:53:20 -0700

    updated to add controls

--- ESP32/calculateDynamics.py
+++ ESP32/calculateDynamics.py
@@ -1,1 +1,40 @@
+import _thread
 from sympy import *
+from time import sleep
+from machine import Pin, PWM, Timer, ADC
+import numpy as np
+
+""" LIMIT SWITCH SETUP """
+LS1 = Pin(4, Pin.IN, Pin.PULL_UP)
+LS2 = Pin(16, Pin.IN, Pin.PULL_UP)
+
+""" GANTRY (HORIZONTAL) MOTOR SETUP """
+IN1 = Pin(25, mode=Pin.OUT)
+IN2 = Pin(26, mode=Pin.OUT)
+# theoretically 70 percent is optimal motor power for running long term
+maxMotorPower = 99
+speed_as_percent = 0
+motor = PWM(IN1, freq=25000, duty_u16=0)  # clockwise is motor high rev_0
+rev_motor = PWM(IN2, freq=25000)
+
+def moveMotorRight(speed):
+    motor.duty(int(0))
+    rev_motor.duty(int(speed))
+    
+def moveMotorLeft(speed):
+    motor.duty(int(speed))
+    rev_motor.duty(int(0))
+
+def calculateDynamics(pos_x,pos_x_last,t_last,checkWatchDog,x_des):
+    Kp = 0.3
+    if checkWatchDog == False:
+        dx = pos_x - pos_x_last/t_last
+        u = Kp*x_des - (Kp*pos_x + dx)
+        if u < 0:
+            moveMotorLeft(u*1000) #assuming ~1000 is max PWM
+        elif u > 0:
+            moveMotorRight(u*1000) #assuming ~1000 is max PWM
+        else:
+            return
+    if checkWatchDog == True:
+        return
--- ESP32/checkWatchDog.py
+++ ESP32/checkWatchDog.py
@@ -1,5 +1,10 @@
-def checkWatchDog(LS3, LS4)
-    if LS3==True | LS4==True
-        return false
-    else
-        return true
+from machine import Pin, PWM, Timer, ADC, PWM
+
+LS1 = Pin(4, Pin.IN, Pin.PULL_UP)
+LS2 = Pin(16, Pin.IN, Pin.PULL_UP)
+
+def checkWatchDog(switchState): #take in 4x1 vector of states, return T if hit
+    if switchState == True:
+        return True
+    else:
+        return False
